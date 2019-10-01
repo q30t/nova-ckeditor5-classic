282737a7919267db129212d87cf9c57c0a52d602
Jérémy Gasche <jeremy@hawaii.do>
2019-10-01 12:09:34 +0200

    add link option

--- resources/js/components/FormField.vue
+++ resources/js/components/FormField.vue
@@ -17,10 +17,10 @@
 </template>
 
 <script>
-import { FormField, HandlesValidationErrors } from 'laravel-nova'
-import CKEditor from '@ckeditor/ckeditor5-vue'
-import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
-import NovaCKEditor5UploadAdapter from '../ckeditor5/upload-adapter'
+import { FormField, HandlesValidationErrors } from "laravel-nova";
+import CKEditor from "@ckeditor/ckeditor5-vue";
+import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
+import NovaCKEditor5UploadAdapter from "../ckeditor5/upload-adapter";
 
 export default {
     mixins: [FormField, HandlesValidationErrors],
@@ -29,9 +29,9 @@ export default {
         ckeditor: CKEditor.component
     },
 
-    props: ['resourceName', 'resourceId', 'field'],
+    props: ["resourceName", "resourceId", "field"],
 
-    data () {
+    data() {
         return {
             editor: ClassicEditor,
             defaultEditorConfig: {
@@ -40,44 +40,43 @@ export default {
                     field: this.field,
                     draftId: uuidv4()
                 },
-                language: 'en',
+                language: "en",
                 toolbar: this.field.options.toolbar,
                 heading: this.field.options.heading,
                 image: this.field.options.image,
                 fontFamily: this.field.options.fontFamily,
-                extraPlugins: [
-                    this.createUploadAdapterPlugin
-                ]
+                extraPlugins: [this.createUploadAdapterPlugin],
+                link: this.field.options.link
             }
-        }
+        };
     },
 
     computed: {
-        draftId: function () {
-            return this.defaultEditorConfig.nova.draftId
+        draftId: function() {
+            return this.defaultEditorConfig.nova.draftId;
         },
         editorConfig: function() {
-            let editorConfig = this.defaultEditorConfig
+            let editorConfig = this.defaultEditorConfig;
 
-            if (! editorConfig.nova.field.withFiles) {
+            if (!editorConfig.nova.field.withFiles) {
                 editorConfig.removePlugins = [
-                    'Image',
-                    'ImageToolbar',
-                    'ImageCaption',
-                    'ImageStyle',
-                    'ImageTextAlternate',
-                    'ImageUpload'
-                ]
-                editorConfig.image = {}
-                editorConfig.extraPlugins = []
+                    "Image",
+                    "ImageToolbar",
+                    "ImageCaption",
+                    "ImageStyle",
+                    "ImageTextAlternate",
+                    "ImageUpload"
+                ];
+                editorConfig.image = {};
+                editorConfig.extraPlugins = [];
             }
 
-            return editorConfig
+            return editorConfig;
         }
     },
 
     beforeDestroy() {
-        this.cleanUp()
+        this.cleanUp();
     },
 
     methods: {
@@ -92,33 +91,40 @@ export default {
          * Set the editor initial, internal value for the field when the editor is ready.
          */
         setEditorInitialValue(editor) {
-            this.value = this.field.value || ''
+            this.value = this.field.value || "";
         },
 
         /**
          * Fill the given FormData object with the field's internal value.
          */
         fill(formData) {
-            formData.append(this.field.attribute, this.value || '')
-            formData.append(this.field.attribute + 'DraftId', this.draftId);
+            formData.append(this.field.attribute, this.value || "");
+            formData.append(this.field.attribute + "DraftId", this.draftId);
         },
 
         /**
          * Update the field's internal value.
          */
         handleChange(value) {
-            this.value = value
+            this.value = value;
         },
 
         /**
-        * Create CKEditor upload adapter plugin.
-        */
+         * Create CKEditor upload adapter plugin.
+         */
         createUploadAdapterPlugin(editor) {
-            let novaConfig = editor.config.get('nova')
+            let novaConfig = editor.config.get("nova");
 
-            editor.plugins.get('FileRepository').createUploadAdapter = (loader) => {
-                return new NovaCKEditor5UploadAdapter(loader, novaConfig.resourceName, novaConfig.field, novaConfig.draftId)
-            }
+            editor.plugins.get(
+                "FileRepository"
+            ).createUploadAdapter = loader => {
+                return new NovaCKEditor5UploadAdapter(
+                    loader,
+                    novaConfig.resourceName,
+                    novaConfig.field,
+                    novaConfig.draftId
+                );
+            };
         },
 
         /**
@@ -131,16 +137,18 @@ export default {
                         `/nova-vendor/ckeditor5-classic/${this.resourceName}/attachments/${this.field.attribute}/${this.draftId}`
                     )
                     .then(response => console.log(response))
-                    .catch(error => {})
+                    .catch(error => {});
             }
         }
-
     }
-}
+};
 
 function uuidv4() {
     return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-        (c ^ (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))).toString(16)
-    )
+        (
+            c ^
+            (crypto.getRandomValues(new Uint8Array(1))[0] & (15 >> (c / 4)))
+        ).toString(16)
+    );
 }
 </script>
